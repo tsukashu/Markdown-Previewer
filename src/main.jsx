4e0b5f01ef998d6448c256a52bd529dbd4901ee4
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-06-28 15:23:46 +0900

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
-import AppUseClass from './AppUseClass';
-import AppExample from './AppExample';
-import './index.css'
+import App from './App';
+// import AppUseClass from './AppUseClass';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <AppUseClass />
-    {/* <AppExample /> */}
+    <App />
+    {/* <AppUseClass /> */}
   </React.StrictMode>
 );
