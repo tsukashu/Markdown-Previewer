f0be13af46f8e5cf1932644426be9b7a4c829d09
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-07-05 13:22:44 +0900

    update some

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import DOMPurify from 'dompurify';
-// import { marked } from 'marked';
 import { useState } from 'react';
 import './App.css';
 import placeholder from './placeholder';
@@ -8,30 +6,22 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
 
-// const test = `Editor Function`;
-
 const App = () => {
   const [text, setText] = useState(placeholder);
-  // const [md, setMd] = useState(ConvertMD(text));
 
   const handleChange = (event) => {
     setText(event.target.value);
-  };
-
-  const handleClick = () => {
-    setText(placeholder);
   };
 
   return (
     <div className='App'>
       <div className='App-container'>
         <h2>Markdown Previewer</h2>
-
+        <div>
+          <button onClick={() => setText(placeholder)}>RESET</button>
+        </div>
         <div className='App-contents'>
-          <div>
-            <div>
-              <button onClick={handleClick}>RESET</button>
-            </div>
+          <div className='resizable'>
             <textarea
               id='editor'
               value={text}
@@ -39,36 +29,19 @@ const App = () => {
               maxLength={10000}
             ></textarea>
           </div>
-          <div id='preview'>
-            <ReactMarkdown
-              children={text}
-              remarkPlugins={[remarkGfm, remarkBreaks]}
-            />
-            {/* <div
-              id='preview'
-              dangerouslySetInnerHTML={{ __html: ConvertMD(text) }}
-            /> */}
+
+          <div className='resizable'>
+            <div id='preview'>
+              <ReactMarkdown
+                children={text}
+                remarkPlugins={[remarkGfm, remarkBreaks]}
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-// const ConvertMD = (raw) => {
-//   marked.setOptions({
-//     renderer: new marked.Renderer(),
-//     // highlight: function(code, lang) {
-//     //   const hljs = require('highlight.js');
-//     //   const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-//     //   return hljs.highlight(code, { language }).value;
-//     // },
-//     // langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
-//     gfm: true,
-//     breaks: true,
-//     sanitizer: DOMPurify.sanitize(),
-//   });
-//   return marked.parse(raw);
-// };
 
 export default App;
